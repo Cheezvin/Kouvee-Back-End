2b11470b9dd5363a0bfcc7ff57f4f03414677aea
Cheezvin <melvin.simahan7@gmail.com>
2020-04-17 13:11:57 +0700

    add telp to transaksi pembayaran

--- app/TransaksiPembayaran.php
+++ app/TransaksiPembayaran.php
@@ -11,6 +11,7 @@ class TransaksiPembayaran extends Model
                             'tanggal',
                             'total_harga',
                             'nama_customer',
+                            'telp_customer',
                             'status',
                             'logAktor',
                             'logAksi',

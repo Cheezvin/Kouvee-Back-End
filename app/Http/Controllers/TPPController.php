cc21b82e014349823329c3b78a0819bcf6d7444a
Cheezvin <melvin.simahan7@gmail.com>
2020-04-22 16:37:12 +0700

    perbaikan

--- app/Http/Controllers/TPPController.php
+++ app/Http/Controllers/TPPController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\TPP;
-use PDF;
+use \PDF;
 
 class TPPController extends Controller
 {
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -13,6 +13,9 @@ class ComposerAutoloaderInitd1e33b5b1b8a867ddb0ab77eefeb74b5
         }
     }
 
+    /**
+     * @return \Composer\Autoload\ClassLoader
+     */
     public static function getLoader()
     {
         if (null !== self::$loader) {

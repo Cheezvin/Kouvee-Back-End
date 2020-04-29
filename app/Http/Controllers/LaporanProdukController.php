916973b8cd276b713c523ce419c248a565ab2df8
Cheezvin <melvin.simahan7@gmail.com>
2020-04-29 13:17:20 +0700

    try laporan produk terlaris 23

--- app/Http/Controllers/LaporanProdukController.php
+++ app/Http/Controllers/LaporanProdukController.php
@@ -78,10 +78,10 @@ class LaporanProdukController extends Controller
             $temp = LaporanProduk::where($max)->max('jumlah_terjual');
             if($temp != null) {
                 $where=['tahun' => $tahun, 'bulan' => $bulan[$x],'jumlah_terjual' => LaporanProduk::where($max)->max('jumlah_terjual')];
-                array_push($data,LaporanProduk::where($where)->get());
+                array_push($data,LaporanProduk::where($where)->firstOrFail());
             }
         }
-        return json_encode($data);
+        return $data;
         
     }
 

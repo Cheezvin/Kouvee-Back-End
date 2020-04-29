8e9512142787f9a1ddfb600979c713b5e8e05fcf
Cheezvin <melvin.simahan7@gmail.com>
2020-04-29 21:29:59 +0700

    add laporan pendapatan pertahun 2

--- app/Http/Controllers/LaporanLayananController.php
+++ app/Http/Controllers/LaporanLayananController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\LaporanLayanan;
+use App\LaporanProduk;
 use \PDF;
 
 class LaporanLayananController extends Controller

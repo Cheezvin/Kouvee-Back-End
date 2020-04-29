b3b84cf02e0c6909e273a56ab64991e8d9e03ae6
Cheezvin <melvin.simahan7@gmail.com>
2020-04-30 02:34:19 +0700

    fixing array 4

--- app/Http/Controllers/LaporanLayananController.php
+++ app/Http/Controllers/LaporanLayananController.php
@@ -96,7 +96,7 @@ class LaporanLayananController extends Controller
     public function totalPenjualan($tahun)
     {
         $bulan = ["Januari","Februari","Maret","April","Mei","Juni","Juli","Agustus","September","Oktober","November","Desember"];
-        $data[] = null;
+        $data = [];
         for ($x = 0; $x < 12; $x++) {
             $max = ['tahun' => $tahun,'bulan' => $bulan[$x]];
             $temp2 = LaporanLayanan::where($max)->sum('total_penjualan');
